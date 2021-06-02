8c1204cd1da6b465d1d6cc6c33b4fd14ad6c2227
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2021-06-02 23:35:01 +0530

    first commit

---
+++ components/ClientRouter.js
@@ -0,0 +1,10 @@
+import {ClientRouter as AppBridgeClientRouter} from '@shopify/app-bridge-react';
+import { withRouter } from 'next/router';
+
+function ClientRouter(props) {
+  const {router} = props;
+  console.log('ClientRouter', props);
+  return <AppBridgeClientRouter history={router} />;
+};
+
+export default withRouter(ClientRouter);
---
+++ pages/annotated-layout.js
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Card,
+  Form,
+  FormLayout,
+  Layout,
+  Page,
+  Stack,
+  TextField,
+} from '@shopify/polaris';
+
+import React from 'react';
+
+class AnnotatedLayout extends React.Component {
+  state = {
+    discount: '10%',
+  };
+
+  render() {
+    const { discount } = this.state;
+
+    return (
+      <Page>
+        <Layout>
+          <Layout.AnnotatedSection
+            title="Default discount"
+            description="Add a product to Sample App, it will automatically be discounted."
+          >
+            <Card sectioned>
+              <Form onSubmit={this.handleSubmit}>
+                <FormLayout>
+                  <TextField
+                    value={discount}
+                    onChange={this.handleChange('discount')}
+                    label="Discount percentage"
+                    type="discount"
+                  />
+                  <Stack distribution="trailing">
+                    <Button primary submit>
+                      Save
+                    </Button>
+                  </Stack>
+                </FormLayout>
+              </Form>
+            </Card>
+          </Layout.AnnotatedSection>
+        </Layout>
+      </Page>
+    );
+  }
+
+  handleSubmit = () => {
+    this.setState({
+      discount: this.state.discount,
+    });
+    console.log('submission', this.state);
+  };
+
+  handleChange = (field) => {
+    return (value) => this.setState({ [field]: value });
+  };
+}
+
+export default AnnotatedLayout;
